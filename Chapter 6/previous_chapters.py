77089ebddee5af38102db037d231387b58041e6f
youssef-omarrr <es-youssef.moh.ah2026@alexu.edu.eg>
2025-08-06 18:11:58 +0300

    added ui in chapter 6

--- Chapter 6/previous_chapters.py
+++ Chapter 6/previous_chapters.py
@@ -318,3 +318,42 @@ def text_to_token_ids(text, tokenizer):
 def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0)  # remove batch dimension
     return tokenizer.decode(flat.tolist())
+
+
+
+
+
+
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
+    model.eval()
+
+    # Prepare inputs to the model
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[0]
+    # Note: In the book, this was originally written as pos_emb.weight.shape[1] by mistake
+    # It didn't break the code but would have caused unnecessary truncation (to 768 instead of 1024)
+
+    # Truncate sequences if they too long
+    input_ids = input_ids[:min(max_length, supported_context_length)]
+    assert max_length is not None, (
+        "max_length must be specified. If you want to use the full model context, "
+        "pass max_length=model.pos_emb.weight.shape[0]."
+    )
+    assert max_length <= supported_context_length, (
+        f"max_length ({max_length}) exceeds model's supported context length ({supported_context_length})."
+    )    
+    # Alternatively, a more robust version is the following one, which handles the max_length=None case better
+    # max_len = min(max_length,supported_context_length) if max_length else supported_context_length
+    # input_ids = input_ids[:max_len]
+    
+    # Pad sequences to the longest sequence
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0) # add batch dimension
+
+    # Model inference
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]  # Logits of the last output token
+    predicted_label = torch.argmax(logits, dim=-1).item()
+
+    # Return the classified result
+    return "spam" if predicted_label == 1 else "not spam"
